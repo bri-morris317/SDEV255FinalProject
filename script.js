5815dbc1b1c21f6187b9ab70115c85505324e042
bri-morris317 <104736234+bri-morris317@users.noreply.github.com>
2024-04-28 09:16:15 -0400

    Update script.js for Render

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
